235d684c5446c977334a37f08cb8c9dbb4f36c82
Zmeev06 <se.rg.ey06@icloud.com>
2025-03-20 12:21:41 +0300

    add cancelPrevious

--- src/useApiClient/useApiClient.ts
+++ src/useApiClient/useApiClient.ts
@@ -7,6 +7,11 @@ interface ApiConfig {
     cacheTime?: number;
 }
 
+type RequestSettings = {
+    useCache?: boolean;
+    cancelPrevious?: boolean;
+};
+
 const globalConfig = ref<ApiConfig>({});
 const cache = new Map<string, { data: any; timestamp: number }>();
 
@@ -34,15 +39,27 @@ export function useApiClient<T = any>() {
     const status = ref<number | null>(null);
     const isLoading = ref<boolean>(false);
 
-    const request = async (config: AxiosRequestConfig, useCache = true) => {
+    let controller: AbortController | null = null;
+
+    const request = async (
+        config: AxiosRequestConfig,
+        settings: RequestSettings = { useCache: true, cancelPrevious: false }
+    ) => {
         isLoading.value = true;
         error.value = null;
+
+        if (settings.cancelPrevious && controller) {
+            controller.abort();
+        }
+
+        controller = new AbortController();
+        config.signal = controller.signal;
 
         const cacheKey = JSON.stringify({ url: config.url, params: config.params });
         const cacheEntry = cache.get(cacheKey);
         const cacheTime = globalConfig.value.cacheTime ?? 60000;
 
-        if (useCache && cacheEntry && Date.now() - cacheEntry.timestamp < cacheTime) {
+        if (settings.useCache && cacheEntry && Date.now() - cacheEntry.timestamp < cacheTime) {
             data.value = cacheEntry.data;
             isLoading.value = false;
             return;
@@ -53,10 +70,15 @@ export function useApiClient<T = any>() {
             data.value = response.data;
             status.value = response.status;
 
-            if (useCache) {
+            if (settings.useCache) {
                 cache.set(cacheKey, { data: response.data, timestamp: Date.now() });
             }
         } catch (err) {
+            if (axios.isCancel(err)) {
+                console.log("Запрос отменён:", config.url);
+                return;
+            }
+
             const axiosError = err as AxiosError;
             error.value = (axiosError.response?.data as string) ?? "Unknown error";
             status.value = axiosError.response?.status ?? null;
